24ee8a56e1f644faadd08d00ccfa8c712a3ee1dc
HosamHasanRamadan <hosam.hasan.ramadan@gmail.com>
2022-09-30 23:37:35 +0200

    config share handler ios

---
+++ ios/ShareExtension/ShareViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+//
+//  Created by Hosam Hasan on 30/09/2022.
+//
+
+import UIKit
+import Social
+import share_handler_ios_models
+
+class ShareViewController: ShareHandlerIosViewController {}
